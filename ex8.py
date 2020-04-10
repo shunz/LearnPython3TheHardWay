b0bf9e2e1c85efcbb0e6b6d10eca5b4e421c8ee8
shunz <rockucn@gmail.com>
2020-04-10 22:32:23 +0800

    Exercise 8: Printing, Printing

---
+++ ex8.py
@@ -0,0 +1,15 @@
+# ex8.py
+# Exercise 8: Printing, Printing
+
+formatter = "{} {} {} {}"
+
+print(formatter.format(1, 2, 3, 4))
+print(formatter.format("one", "two", "three", "four"))
+print(formatter.format(True, False, False, True))
+print(formatter.format(formatter, formatter, formatter, formatter))
+print(formatter.format(
+    "Try your",
+    "Own text here",
+    "maybe a poem",
+    "or a song about fear"
+))

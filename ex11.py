b88d9d2b2475f6931bdc9186fadf5a26141c46f7
shunz <rockucn@gmail.com>
2020-04-11 17:20:45 +0800

    Exercise 11: Asking Questions

---
+++ ex11.py
@@ -0,0 +1,11 @@
+# Exercise 11: Asking Questions
+
+print("How old are you?", end=' ')
+age = input()
+print("How tall are you?", end=' ')
+height = input()
+print("how much do you wieght?", end=' ')
+weight = input()
+
+
+print(f"So, you're {age } old, {height} tall and {weight} heavy.")

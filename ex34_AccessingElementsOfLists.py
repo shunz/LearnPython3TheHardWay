6bf81d47a0e6e9315c104d7c5818eb0ecfd983a1
shunz <rockucn@gmail.com>
2020-05-02 11:40:23 +0800

    Exercise 34: Accessing Elements of Lists

---
+++ ex34_AccessingElementsOfLists.py
@@ -0,0 +1,9 @@
+# Exercise 34: Accessing Elements of Lists
+
+animals = ['bear', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+print(f"At position 0 is the 1st animal, which is a {animals[0]}.")
+print(f"At position 1 is the 2nd animal, which is a {animals[1]}.")
+print(f"At position 2 is the 3nd animal, which is a {animals[2]}.")
+print(f"At position -1 is the last animal, which is a {animals[-1]}.")
+

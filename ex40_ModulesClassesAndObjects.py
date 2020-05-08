478c12459a7094cf3c9c8296495884b36fab4b6a
shunz <rockucn@gmail.com>
2020-05-08 23:36:45 +0800

    Exercise 40: Modules, Classes and Objects

---
+++ ex40_ModulesClassesAndObjects.py
@@ -0,0 +1,8 @@
+# Exercise 40: Modules, Classes and Objects
+
+mystuff = {'apple': "I AM APPLES!"}
+print(mystuff['apple'])  # get apple from dict
+
+import ex40_mystuff
+ex40_mystuff.apple()  # get apple frome module
+print(ex40_mystuff.tangerine)  # same thing, it's just a variable.
---
+++ ex40_mystuff.py
@@ -0,0 +1,7 @@
+# this gose in mystuff.py
+def apple():
+    print("I AM APPLES!")
+    
+
+# this is just a variable
+tangerine = "Living reflection of a dream."

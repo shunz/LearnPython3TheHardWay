4a03ea7ca03d7ebcc4aafea6d124a216e0552020
shunz <rockucn@gmail.com>
2020-05-09 23:49:59 +0800

    Exercise 41: Learning to Speak Object Oriented

---
+++ ex41_LearningToSpeakObjectOriented.py
@@ -0,0 +1,17 @@
+# Exercise 41: Learning to Speak Object Oriented
+
+"""学习面向对象术语
+
+专有词汇练习
+ • 类(class): 告诉Python创建新类型的东西
+ • 对象(object): 两个意思，即最基本的东西，或者某样东西的实例。
+ • 实例(instance): 这是让Python创建一个类时得到的东西。
+ • def: 这是在类里边定义函数的方法。
+ • self: 在类的函数中，self指代被访问的对象或者实例的一个变量
+ • 继承(inheritance): 指一个类可以继承另一个类的特性，和父子关系类似
+ • 组合(composition): 指一个类可以将别的类作为它的部件构建起来，有点像车子和车轮的关系
+ • 属性(attribute): 类的一个属性，来自于组合，通常是一个变量。
+ • 是什么(is-a): 用来描述继承关系，如Salmon is-a Fish
+ • 有什么(has-a): 用来描述某个东西是由另外一些东西组成的，或者某个东西有某个特征，如
+   Salmon has-a mouth
+"""
